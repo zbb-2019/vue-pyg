614b27117ad40aa98843adc222b0213e6d46ed30
zbb <1102344710@qq.com>
2019-09-10 14:00:27 +0800

    vue-pyg项目初始化

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+//按需导入mint-ui
+import {Header} from 'mint-ui';
+
+Vue.component(Header.name, Header);
+
+//导入mui
+import './lib/mui/css/mui.min.css';
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
