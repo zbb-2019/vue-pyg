a0b22ebb4605bc6427342ebbff9f6cd140382641
zbb <1102344710@qq.com>
2019-09-11 09:30:03 +0800

    完成：九宫格

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,8 +4,29 @@
             <mt-swipe-item v-for="item in lunbotulist" :key="item.id">
                 <img :src="item.img"/>
             </mt-swipe-item>
-
         </mt-swipe>
+
+
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../public/img/icons/apple-touch-icon-120x120.png">
+                <div class="mui-media-body">新闻资讯</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../public/img/icons/apple-touch-icon-120x120.png">
+                <div class="mui-media-body">图片分享</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../public/img/icons/apple-touch-icon-120x120.png">
+                <div class="mui-media-body">商品购买</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../public/img/icons/apple-touch-icon-120x120.png">
+                <div class="mui-media-body">留言反馈</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../public/img/icons/apple-touch-icon-120x120.png">
+                <div class="mui-media-body">视频专区</div></a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <img src="../../public/img/icons/apple-touch-icon-120x120.png">
+                <div class="mui-media-body">联系我们</div></a></li>
+        </ul>
     </div>
 </template>
 
@@ -59,6 +80,19 @@
                 width: 100%;
                 height: 100%;
             }
+        }
+    }
+    ul.mui-grid-view{
+        background-color: #fff;
+    }
+    .mui-grid-view.mui-grid-9 .mui-table-view-cell{
+        div{
+            font-size: 12px;
+        }
+        border: none;
+        img{
+            width: 60px;
+            height: 60px;
         }
     }
 </style>
